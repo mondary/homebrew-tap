35128306d5d1b52180f6930cd748e907d120cc74
cmondary <clement.mondary@gmail.com>
2026-09-15 18:32:50 +0200

    MAJ: sha256 du dmg pk-voice-cloner 0.6.0 (signature developpeur)

--- Casks/pk-voice-cloner.rb
+++ Casks/pk-voice-cloner.rb
@@ -1,6 +1,6 @@
 cask "pk-voice-cloner" do
   version "0.6.0"
-  sha256 "2027db5768f25d05b7976313f4c973cd450de4979de822ae8ed68860960a3b2f"
+  sha256 "5c843eb8a4a47fd241231139f4c6288f5e0d5386af9650d6bd0f96fcfc206b02"
 
   url "https://github.com/mondary/Macos_PKvoicecloner/releases/download/v#{version}/PKVoiceCloner-#{version}.dmg"
   name "PK Voice Cloner"
