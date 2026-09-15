6d585fec10e526fb0cd903568b43589ba57351b6
cmondary <clement.mondary@gmail.com>
2026-09-15 19:22:21 +0200

    MAJ: synchroniser le hash du dmg pk

--- Casks/monocode-pk.rb
+++ Casks/monocode-pk.rb
@@ -1,6 +1,6 @@
 cask "monocode-pk" do
   version "2026.09.39"
-  sha256 "2bc72f54c6e9e05fc58797cfca6a346fa7aed8cd59ba670f77162b2dd0c8eff9"
+  sha256 "7cb18d3cc19d6c15d444b9aa5610ab9759edcc82050dc203f7f14498570aa27f"
 
   url "https://github.com/mondary/monocode/releases/download/pk-#{version}/MonoCodePK_#{version}_aarch64.dmg"
   name "MonoCode PK"
